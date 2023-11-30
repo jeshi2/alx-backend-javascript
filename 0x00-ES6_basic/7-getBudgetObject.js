42d6ae371dba3a3932704811e400930e54e89d1e
jeshi2 <jamesvean19@gmail.com>
2023-11-30 09:37:21 +0300

    es6 basics

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
     const budget = {
-        income: income,
-        gdp: gdp,
-        capita: capita,
+      income: income,
+      gdp: gdp,
+      capita: capita,
     };
-
-    return JSON.stringify(budget, null, 2);
-}
+  
+    return budget;
+  }
