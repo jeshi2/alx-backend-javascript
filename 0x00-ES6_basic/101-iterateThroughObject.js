d3c20de57cc5b00917a123bfae60d99ebd7e3e04
jeshi2 <jamesvean19@gmail.com>
2023-11-30 10:05:12 +0300

    es6 basics

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+    const result = [];
+  
+    for (const item of reportWithIterator) {
+      result.push(item);
+    }
+  
+    return result.join(' | ');
+}
