cc4473dbec573d3049bfd234c30318dc056414b8
jeshi2 <jamesvean19@gmail.com>
2023-12-01 08:43:57 +0300

    es6 promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((values) => {
-      values.map((res) => (
-        {
-          status: res.status,
-          value: res.status === 'fulfilled' ? res.value : `Error: ${res.reason.message}`,
-        }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      } else {
+        return { status: 'rejected', value: result.reason };
+      }
     });
+  });
 }
