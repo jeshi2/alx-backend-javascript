3662cb49058cf319123c339eaf234d5b9bc8f0b5
jeshi2 <jamesvean19@gmail.com>
2023-12-01 08:02:43 +0300

    es6 promises

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,15 +2,13 @@ import divideFunction from './8-try';
 
 export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.message);
+    const res = mathFunction();
+    queue.push(res);
+  } catch (err) {
+    queue.push(err.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
