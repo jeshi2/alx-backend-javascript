8c13a756e678546ac2ddefc4c8b43c290e6b38dc
jeshi2 <jamesvean19@gmail.com>
2023-12-03 21:36:49 +0300

    es6 basics lint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,21 +1,10 @@
-function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch((error) => {
-      console.error();
-
-      return {
-        status: 500,
-        body: 'error',
-        error: error.message,
-      };
-    });
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => ({ 
+    status: 200, 
+    body: 'success' 
+  }))
+  .catch(() => Error())
+  .finally(() => { 
+    console.log('Got a response from the API'); 
+  });
 }
-
-export default handleResponseFromAPI;
