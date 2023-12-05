99f21ba7670dad3ca28e5d81ebea13e8a2362fb9
jeshi2 <jamesvean19@gmail.com>
2023-12-05 23:22:40 +0300

    es6 classes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    const newCar = new Car(_brand, _motor, _color);
+    return newCar;
+  }
+}
