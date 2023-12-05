ee34517181bc313d5838ed8e69495183d9f23ea0
jeshi2 <jamesvean19@gmail.com>
2023-12-05 22:19:36 +0300

    es6 classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,52 +1,38 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, 'Name');
-    this._length = this.validateNumber(length, 'Length');
-    this._students = this.validateArray(students, 'Students');
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    this._name = this.validateString(newName, 'Name');
+  set name(val) {
+    if (typeof val !== 'string') { throw new TypeError('Name must be a string'); }
+    this._name = val;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(newLength) {
-    this._length = this.validateNumber(newLength, 'Length');
+  set length(val) {
+    if (typeof val !== 'number') { throw new TypeError('Length must be a number'); }
+    this._length = val;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(newStudents) {
-    this._students = this.validateArray(newStudents, 'Students');
-  }
-
-  validateString(value, attribute) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attribute} must be a string`);
+  set students(val) {
+    if (!Array.isArray(val)) { throw new TypeError('Students must be an array'); }
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < val.length; i++) {
+      if (typeof val[i] !== 'string') { throw new TypeError('Students must be an array of strings'); }
     }
-    return value;
-  }
-
-  validateNumber(value, attribute) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attribute} must be a number`);
-    }
-    return value;
-  }
-
-  validateArray(value, attribute) {
-    if (!Array.isArray(value)) {
-      throw new TypeError(`${attribute} must be an array`);
-    }
-    return value;
+    this._students = val;
   }
 }
