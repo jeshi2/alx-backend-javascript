213623e09273eef5d5ffa97268a6799eced7268d
jeshi2 <jamesvean19@gmail.com>
2023-12-12 15:59:38 +0300

    typescript

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,6 +1,6 @@
 namespace Subjects {
     export class Subject {
-      private _teacher: Teacher;
+      protected _teacher: Teacher;
   
       setTeacher(teacher: Teacher): void {
         this._teacher = teacher;
