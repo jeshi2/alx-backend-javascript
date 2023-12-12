a4384af2e75abae6a1fca80be84a880936c5869d
jeshi2 <jamesvean19@gmail.com>
2023-12-12 15:39:11 +0300

    typescript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,48 @@ const director1: Directors = {
 };
 
 console.log(createDirector(director1));
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstLetter}. ${lastName}`;
+  return fullName;
+};
+
+const teacherName = printTeacher('John', 'Doe');
+console.log(teacherName);
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface extends Person {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  readonly firstName: string;
+  readonly lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('John', 'Doe');
+console.log(student.workOnHomework()); 
+console.log(student.displayName()); 
