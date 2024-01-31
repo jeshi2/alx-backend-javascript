464f8199d7263da084eee3253e66bc7e35e76b80
jeshi2 <jamesvean19@gmail.com>
2024-01-31 15:58:55 +0300

    che

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+const app = http.createServer((_req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!\n');
 });
 
 app.listen(1245);
