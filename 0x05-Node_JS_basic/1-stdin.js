fc3b11cc3983e54190eaf90371939d53b9bdbc0c
jeshi2 <jamesvean19@gmail.com>
2024-01-31 14:09:10 +0300

    stdi

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.on('exit', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
