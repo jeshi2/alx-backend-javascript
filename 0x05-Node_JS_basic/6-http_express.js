3d7e3979e840c6ea0651fc605541bb3f4d372233
jeshi2 <jamesvean19@gmail.com>
2024-02-01 13:40:35 +0300

    wel

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!\n');
 });
 
-app.listen(1245);
+app.listen(port);
 
 module.exports = app;
