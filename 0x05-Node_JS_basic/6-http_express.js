caeee312ffc0df2f253b228673d58e73565aeab1
jeshi2 <jamesvean19@gmail.com>
2024-02-01 13:38:01 +0300

    express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello Holberton School!\n');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+app.listen(port);
 
 module.exports = app;
