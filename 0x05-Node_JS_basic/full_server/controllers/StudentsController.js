bad4fad3decaf99b9f27ea3090c953ebdf64692e
jeshi2 <jamesvean19@gmail.com>
2024-02-03 21:37:08 +0300

    fix

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,10 +7,10 @@ const VALID_MAJORS = ['CS', 'SWE'];
 
 /**
  * Contains the student-related route handlers.
- * 
+ *
  */
 class StudentsController {
-  static getAllStudents(request, response) {
+  static getAllStudents (request, response) {
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
 
     readDatabase(dataPath)
@@ -32,7 +32,7 @@ class StudentsController {
           responseParts.push([
             `Number of students in ${field}: ${group.length}.`,
             'List:',
-            group.map((student) => student.firstname).join(', '),
+            group.map((student) => student.firstname).join(', ')
           ].join(' '));
         }
         response.status(200).send(responseParts.join('\n'));
@@ -44,7 +44,7 @@ class StudentsController {
       });
   }
 
-  static getAllStudentsByMajor(request, response) {
+  static getAllStudentsByMajor (request, response) {
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
     const { major } = request.params;
 
