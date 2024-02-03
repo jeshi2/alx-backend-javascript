320adb0c534298e56970e164ff5b79fdee183bb3
jeshi2 <jamesvean19@gmail.com>
2024-02-03 21:27:20 +0300

    we

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -26,15 +26,13 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
         const studentGroups = {};
         const dbFieldNames = fileLines[0].split(',');
         const studentPropNames = dbFieldNames.slice(
-          0,
-          dbFieldNames.length - 1
+          0, dbFieldNames.length - 1
         );
 
         for (const line of fileLines.slice(1)) {
           const studentRecord = line.split(',');
           const studentPropValues = studentRecord.slice(
-            0,
-            studentRecord.length - 1
+            0, studentRecord.length - 1
           );
           const field = studentRecord[studentRecord.length - 1];
           if (!Object.keys(studentGroups).includes(field)) {
